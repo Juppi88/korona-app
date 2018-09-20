3087aa44f9cc911f7015f57b45c89b628b0a22a5
Tuomo Jauhiainen <tuomo.jauhiainen@wapice.com>
2018-09-20 10:26:52 +0300

    Add bash scripts for starting and stopping a stream.

---
+++ scripts/start-stream.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+buffer_file="/run/user/1000/camera.ffm"
+
+# Remove previous buffer file.
+rm -f $buffer_file
+
+# Start the video server if it's not running.
+if ! pgrep -x "ffserver" > /dev/null
+then
+    ffserver & > /dev/null
+fi
+
+# Start recording.
+ffmpeg -i /dev/video0 http://localhost:8090/camera.ffm & > /dev/null
---
+++ scripts/stop-stream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Kill the stream and the stream server.
+killall ffmpeg > /dev/null
+killall ffserver > /dev/null
